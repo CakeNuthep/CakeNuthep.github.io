292b376ba9b13aefb368833d6191ada5e5253378
Suppachai Nuthep <cake.nuthep@gmail.com>
2025-09-28 13:12:47 +0700

    edit camera

--- resume game/src/characterControls.js
+++ resume game/src/characterControls.js
@@ -370,13 +370,13 @@ class CharacterControls extends Box {
     }
 
     updateCameraTarget() {
-        this.camera.position.x = Math.cos(this.angle) * (this.radius+20);
-        this.camera.position.z = Math.sin(this.angle) * (this.radius+20);
-        this.camera.position.y = 7; // Keep camera elevated
+        this.camera.position.x = Math.cos(this.angle) * (this.radius+15);
+        this.camera.position.z = Math.sin(this.angle) * (this.radius+15);
+        this.camera.position.y = 5; // Keep camera elevated
 
 
         if(this.isColiision || this.onGround){
-            this.cameraTarget.setY(this.model.position.y + 3);
+            this.cameraTarget.setY(this.model.position.y + 4);
         }
 
        
